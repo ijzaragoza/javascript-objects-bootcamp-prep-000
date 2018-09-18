9442dfe4477efc3c94ae4dd875bc4dc5c9a95f42
Ian Zaragoza <ianzaragoza@msn.com>
2018-09-18 16:07:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
 
-return Object.assign({}, playlist, { [artistName]: songTitle })
+return Object.assign({playlist}, { [artistName]: songTitle })
 
 }
